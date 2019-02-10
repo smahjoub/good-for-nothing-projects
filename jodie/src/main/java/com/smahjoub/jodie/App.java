4054e522ee777f7c25d8fe987983806f60afa027
smahjoub <saifeddine.mahjoub@outlook.com>
2019-02-10 13:31:21 +0100

    Add bank account kata

---
+++ jodie/src/main/java/com/smahjoub/jodie/App.java
@@ -0,0 +1,13 @@
+package com.smahjoub.jodie;
+
+/**
+ * Bank account kata
+ * @author smahjoub
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Bank account kata!" );
+    }
+}
